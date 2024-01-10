32fef62284dbb4f0097d0151b37cf8648e3f110b
Thomas Lawless <lawlessptom@gmail.com>
2024-01-10 16:53:15 -0500

    initial

---
+++ NotesAppDemo/Model/Note.swift
@@ -0,0 +1,8 @@
+//
+//  Note.swift
+//  NotesAppDemo
+//
+//  Created by Thomas Lawless III on 1/10/24.
+//
+
+import Foundation
---
+++ NotesAppDemo/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  NotesAppDemo
+//
+//  Created by Thomas Lawless III on 1/10/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
